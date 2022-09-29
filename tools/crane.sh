29ebb3280b029cb3144abbe08869bcfa6a44f512
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2022-09-29 14:11:26 +0900

    tmp

--- tools/crane.sh
+++ tools/crane.sh
@@ -8,6 +8,22 @@ IMAGES=$(cat <<EOF
 tomcat:10.0.0-jdk15-openjdk-buster-zstd
 gcc:10.2.0-esgz
 postgres:12-alpine-esgz
+python:3.9-zstdchunked
+pause:3.6
+nerdctl-ipfs-registry:bb682bc
+ipfs-cluster:v0.14.2
+rabbitmq:3.9.4-zstdchunked
+elasticsearch:7.14.0-zstdchunked
+jenkins:2.60.3-esgz
+rethinkdb:2.3.6-esgz
+ubuntu:20.04-ipfs
+golangci-lint:v1.40.1-esgz
+argoproj-argoexec:v3.0.3-org
+alpine:3.10.2-zstdchunked
+stargz-snapshotter:v0.6.4-dev
+golang:1.18-buster-prioritized-libs-esgz
+gosec:v2.8.0-esgz
+argoproj-workflow-controller:v3.0.3-esgz
 EOF
 )
 
