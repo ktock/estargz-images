6265eee1d7a7c10078faa778203d7462f9464f31
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2022-10-03 15:54:09 +0900

    tmp

--- tools/crane.sh
+++ tools/crane.sh
@@ -10,5 +10,6 @@ EOF
 )
 
 for i in $IMAGES ; do
-    crane copy ${i} ${repo}/${i}-test
+    crane copy --platform=linux/amd64 ${i} ${repo}/${i}-test
+    crane copy --platform=linux/amd64 ${i} ${repo}/estargz-images/${i}-test
 done
