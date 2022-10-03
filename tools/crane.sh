a7ab1131eb4cfec949eda1873cb123265976bd15
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2022-10-03 16:39:37 +0900

    tmp
    tmp

--- tools/crane.sh
+++ tools/crane.sh
@@ -11,5 +11,5 @@ repo=ghcr.io/ktock
 # for i in $IMAGES ; do
 # done
 
-crane copy --platform=linux/amd64 ubuntu:20.04 ${repo}/ubuntutest:1-test || true
-crane copy --platform=linux/amd64 ubuntu:20.04 ${repo}/estargz-images/ubuntutest:1-test || true
+crane copy --platform=linux/amd64 ubuntu:20.04 ${repo}/ubuntutest2:1-test || true
+crane copy --platform=linux/amd64 ubuntu:20.04 ${repo}/estargz-images/ubuntutest2:1-test || true
